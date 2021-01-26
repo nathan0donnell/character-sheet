fdd8399861348e2864b7518cf6845e9430ca3899
nathan0donnell <63211472+nathan0donnell@users.noreply.github.com>
2021-01-26 19:48:06 +0000

    Add files via upload
    - Creation of HTML code 
    - Creation of PHP code
    - New methods in Character Class
    - Updated Character constructor

--- Character.py
+++ Character.py
@@ -1,21 +1,35 @@
 class Character:
-    def __init__(self, name, desc, aliases):
+    def __init__(self, cid,name, desc, aliases):
+        self.cid=cid
         self.name = name
         self.desc = desc
         self.aliases = aliases
+      
         # could add more properties later, these are just basics
-    
+
     # method formalising character data
     def print_details(self):
-        out = "Name: "+self.name +"\nDescription: "+self.desc + "\nAliases: "
+        out = "Name: "+self.name + "\nDescription: "+self.desc + "\nAliases: "
         if self.aliases is list:
             for i in self.aliases:
                 out += "\n - "+i
         elif self.aliases == "N/A":
-            out+= "No Known Aliases"
+            out += "No Known Aliases"
         return out
 
     # method turning data into HTML form
     def htmlify_character(self):
-        out="HTML code here" # add in HTML code translation - labels and textboxes for each property
+        
+        out = "<br /><label for=\""+self.cid+"name\" style=\"margin-right:30px; text-align:right\">Name:</label>\n<input type=\"text\" id=\"name\" name=\""+self.cid+"name\" value=\""
+        out+=self.name
+        out+="\"/><br />\n<label for=\""+self.cid+"desc\" style=\"display:inline-block;height:50px;margin-right:30px ;text-align:right\">Description:</label>\n<textarea name=\""+self.cid+"desc\" rows = \"3\" cols = \"40\" name = \"desc\">"
+        out+=self.desc
+        out+="</textarea><br>"
+        
+        return out
+
+    # method turning data into PHP 
+    def phpify_character(self):
+        out="\nName: <?php echo $_POST[\""+self.cid+"name\"]; ?><br>\n"
+        out+="Description: <?php echo $_POST[\""+self.cid+"desc\"]; ?><br>"
         return out
---
+++ json_to_property_converter.py
@@ -0,0 +1,57 @@
+import json
+
+# opens the JSON file in read mode and returns it as a list
+def open_json(file_path):
+    try:
+      with open(file_path, 'r') as myfile:
+          data = myfile.read()
+      return json.loads(data)
+    except IOError:
+        print("IOError Occurred")
+
+# the character id is found as it is needed for navigating th JSON file
+def get_character_id(file_path):
+    try:
+        character = open_json(file_path)
+        for i in character['entities']:
+            character_id = i
+        return character_id
+    except:
+        return "Error Finding ID"
+
+# the character name, in english, is found in the file and is returned as a string
+def get_character_name(file_path):
+    try:
+        character = open_json(file_path)
+        character_id = get_character_id(file_path)
+        character_name = character['entities'][character_id]['labels']['en']['value']
+        return character_name
+    except:
+        return "N/A"
+
+# the character description, in english, is found in the file and is returned as a string
+def get_character_desc(file_path):
+    try:
+        character = open_json(file_path)
+        character_id = get_character_id(file_path)
+        character_desc = character['entities'][character_id]['descriptions']['en']['value']
+        return character_desc
+    except:
+        return "N/A"
+
+# a list of aliases the character has, in english, is found in the file and is returned as a list of strings
+def get_character_aliases(file_path):
+    try:
+        character = open_json(file_path)
+        character_id = get_character_id(file_path)
+        character_aliases = ["none"]  # needed to define list variable
+        # going through a list of aliases and adding each value to our list
+        for i in range(len(character['entities'][character_id]['aliases']['en'])):
+            if i == 0:
+              character_aliases[0] = character['entities'][character_id]['aliases']['en'][i]['value']
+            else:
+               character_aliases.append(
+               character['entities'][character_id]['aliases']['en'][i]['value'])
+        return character_aliases
+    except:
+        return "N/A"
